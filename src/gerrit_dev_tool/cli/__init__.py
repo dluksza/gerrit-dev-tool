67f5db7283709e68ca9aa09e4eaedc9de21aa154
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-10-28 18:42:47 +0100

    Add executable name to program name
    Change-Id: I4bd870b211b6cfb3bb7a1c060ff7b21f78619400

--- src/gerrit_dev_tool/cli/__init__.py
+++ src/gerrit_dev_tool/cli/__init__.py
@@ -7,6 +7,6 @@ from gerrit_dev_tool.__about__ import __version__
 
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
-@click.version_option(version=__version__, prog_name="Gerrit Dev Tool")
+@click.version_option(version=__version__, prog_name="grdt - Gerrit Dev Tool")
 def gerrit_dev_tool():
     click.echo("Hello world!")
