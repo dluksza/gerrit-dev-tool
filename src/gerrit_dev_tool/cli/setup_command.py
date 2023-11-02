1253738f012c2ce57c854bc182150bf74115d312
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-11-02 22:54:46 +0000

    Initialize Gerrit testsite at the end of setup
    To finish off Gerrit workspace setup we automatically initialize the
    testsite.
    
    Initialization can be skipped by providing `--no-init` flag.
    
    Change-Id: I54e7854bd3c8f1bebcc94e3d89c3c35d9cdfbc51

--- src/gerrit_dev_tool/cli/setup_command.py
+++ src/gerrit_dev_tool/cli/setup_command.py
@@ -1,11 +1,13 @@
 # SPDX-FileCopyrightText: 2023-present Dariusz Luksza <dariusz.luksza@gmail.com>
 #
 # SPDX-License-Identifier: Apache-2.0
+import os
 import click
 
 from gerrit_dev_tool.git_client import GitClient
 from gerrit_dev_tool.bazel_client import BazelClient
 from gerrit_dev_tool.grdt_workspace import GrdtWorkspace
+from gerrit_dev_tool.testsite_client import TestsiteClient
 from gerrit_dev_tool.urls import Urls
 
 
@@ -13,12 +15,13 @@ from gerrit_dev_tool.urls import Urls
 @click.option(
     "-n",
     "--name",
-    default="./gerrit-workspace",
-    type=click.Path(exists=False, file_okay=False),
+    default=os.path.join(os.getcwd(), "gerrit-workspace"),
+    type=click.Path(exists=False, file_okay=False, resolve_path=True),
     help="Name of directory where workspace should be initialised.",
 )
-@click.option("--no-build", is_flag=True, help="Don't build Gerrit during setup.")
-def setup(name, no_build):
+@click.option("--no-build", is_flag=True, help="Don't build Gerrit during setup also implies '--no-init'")
+@click.option("--no-init", is_flag=True, help="Don't initialize Gerrit testsite.")
+def setup(name, no_build, no_init):
     """Setup Gerrit Dev Tool workspace.
 
     Creates a directory structure for Gerrit Dev Tool to operate and clones the Gerrit project.
@@ -30,3 +33,6 @@ def setup(name, no_build):
         bazel = BazelClient(workspace.gerrit())
         bazel.sync()
         bazel.build("gerrit")
+
+    if not no_build or not no_init:
+        TestsiteClient(workspace).init_dev()
---
+++ src/gerrit_dev_tool/testsite_client.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2023-present Dariusz Luksza <dariusz.luksza@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+import subprocess
+
+from gerrit_dev_tool.grdt_workspace import GrdtWorkspace
+
+
+class TestsiteClient(object):
+    def __init__(self, workspace: GrdtWorkspace) -> None:
+        self._worktree = workspace.gerrit()
+        self._testsite = workspace.testsite()
+        self._java_path = None
+
+    def init_dev(self):
+        self._run("init", "--batch", "--no-auto-start", "--dev")
+
+    def reindex(self):
+        self._run("reindex")
+
+    def _run(self, *args):
+        if not self._java_path:
+            output_base = subprocess.check_output(
+                ["bazel", "info", "output_base"],
+                text=True,
+                cwd=self._worktree,
+                stderr=subprocess.DEVNULL,
+            )
+            self._java_path = output_base.strip() + "/external/local_jdk/bin/java"
+
+        subprocess.run(
+            [self._java_path, "-jar", "bazel-bin/gerrit.war"] + list(args) + ["-d", self._testsite],
+            cwd=self._worktree,
+            check=True,
+        )
