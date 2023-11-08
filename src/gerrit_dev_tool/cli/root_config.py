0450df2373e6bdbcc27e42e3b0b965dd6082f5d2
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-11-08 21:51:14 +0000

    Fill in `RootConfig` object
    Add all clients (aka. helper) classes into the root config object.
    
    Change-Id: I6427c4cf8c112c2faf5d1d5793afeb6e055b9e6e

--- src/gerrit_dev_tool/cli/root_config.py
+++ src/gerrit_dev_tool/cli/root_config.py
@@ -3,14 +3,18 @@
 # SPDX-License-Identifier: Apache-2.0
 import click
 
+from gerrit_dev_tool.bazel_client import BazelClient
 from gerrit_dev_tool.gerrit_worktree import GerritWorktree
 from gerrit_dev_tool.grdt_workspace import GrdtWorkspace
+from gerrit_dev_tool.testsite_client import TestsiteClient
 
 
 class RootConfig:
     def __init__(self, workspace: GrdtWorkspace, verbose=False):
         self.workspace = workspace
         self.gerrit_worktree = GerritWorktree(workspace.gerrit)
+        self.bazel = BazelClient(workspace.gerrit)
+        self.site = TestsiteClient(workspace)
         self.verbose = verbose
 
 
