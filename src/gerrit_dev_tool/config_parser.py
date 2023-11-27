d39e3272ff255c36105b4a3d902136578c02c31b
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-11-27 18:04:25 +0000

    Parse and modify gerrit.config
    Adds a `ConfigParser` helper class that parses Gerrit's config files and
    provide API to modify them. It will be needed to automatically add and
    remove plugin configuration.
    
    Change-Id: Idf9474a4a65ed4f14769dbd6a0a1625836d3444c

---
+++ src/gerrit_dev_tool/config_parser.py
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: 2023-present Dariusz Luksza <dariusz.luksza@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import configparser
+
+
+class _MultiDict(dict):
+    def __setitem__(self, key, value):
+        if isinstance(value, list) and key in self:
+            current = self[key]
+            if isinstance(current, list):
+                self[key].extend(value)
+            else:
+                super(_MultiDict, self).__setitem__(key, [current, *value])
+        else:
+            super().__setitem__(key, value)
+
+
+class ConfigParser(configparser.ConfigParser):
+    def __init__(self, allow_no_value=False, interpolation=configparser.BasicInterpolation()):
+        super().__init__(
+            strict=False,
+            dict_type=_MultiDict,
+            comment_prefixes=("#",),
+            inline_comment_prefixes=("#",),
+            empty_lines_in_values=False,
+            interpolation=interpolation,
+            allow_no_value=allow_no_value,
+            converters={"list": lambda v: v.split("\n")},
+        )
+        self._interpolation = interpolation
+        self._allow_no_value = allow_no_value
+
+    def optionxform(self, optionstr: str) -> str:
+        return optionstr
+
+    def write(self, fp, space_around_delimiters=True):
+        if space_around_delimiters:
+            d = " = "
+        else:
+            d = "="
+        for section in self.sections():
+            self._write_section(fp, section, self[section].items(), d)
+
+    def remove_value(self, section_name, option_name, value):
+        section = self[section_name]
+        current = section[option_name]
+        if value == current:
+            section.pop(option_name)
+        if "\n" in current:
+            current_list = current.split("\n")
+            index = current_list.index(value)
+            if index > -1:
+                current_list.pop(index)
+                section[option_name] = "\n".join(current_list)
+
+    def _write_section(self, fp, section_name, section_items, delimiter):
+        fp.write("[{}]\n".format(section_name))
+        for key, value in section_items:
+            if "\n" in value:
+                for v in value.split("\n"):
+                    self._write_value(fp, section_name, key, v, delimiter)
+            else:
+                self._write_value(fp, section_name, key, value, delimiter)
+
+    def _write_value(self, fp, section_name, key, value, delimiter):
+        value = self._interpolation.before_write(self, section_name, key, value)
+        if value is not None or not self._allow_no_value:
+            value = delimiter + str(value).replace('\n', '\n\t')
+        else:
+            value = ""
+        fp.write("\t{}{}\n".format(key, value))
---
+++ tests/config_parser_test.py
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: 2023-present Dariusz Luksza <dariusz.luksza@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from gerrit_dev_tool.config_parser import ConfigParser
+
+example_config = "./tests/gerrit.config"
+
+
+def test_no_op(tmp_path):
+    parser = ConfigParser()
+    output_file = os.path.join(tmp_path, "output.config")
+
+    with open(output_file, "w") as output:
+        parser.read(example_config)
+        parser.write(output)
+
+    with open(example_config) as expected, open(output_file) as actual:
+        assert expected.read() == actual.read()
+
+
+def test_add_list_option(tmp_path):
+    parser = ConfigParser()
+    output_file = os.path.join(tmp_path, "output.config")
+
+    with open(output_file, "w") as output:
+        parser.read(example_config)
+        parser.read("./tests/append_list_item.config")
+        parser.write(output)
+
+    actual = ConfigParser()
+    actual.read(output_file)
+
+    assert len(actual["container"].getlist("javaOptions")) == 3
+
+
+def test_add_section(tmp_path):
+    parser = ConfigParser()
+    output_file = os.path.join(tmp_path, "output.config")
+
+    with open(output_file, "w") as output:
+        parser.read(example_config)
+        parser.read("./tests/append_section.config")
+        parser.write(output)
+
+    actual = ConfigParser()
+    actual.read(output_file)
+
+    assert actual["github"].get("url") == "https://github.com"
+
+
+def test_remove_value_from_list():
+    parser = ConfigParser()
+    parser.read(example_config)
+
+    assert len(parser["container"].getlist("javaOptions")) == 2
+    javaOption = parser["container"].getlist("javaOptions")[0]
+
+    parser.remove_value("container", "javaOptions", javaOption)
+
+    assert len(parser["container"].getlist("javaOptions")) == 1
+
+
+def test_remove_scalar_value():
+    parser = ConfigParser()
+    parser.read(example_config)
+
+    assert "basePath" in parser["gerrit"]
+
+    parser.remove_value("gerrit", "basePath", "git")
+
+    assert not "basePath" in parser["gerrit"]
